556eb08d4d1cf12c772f52c0976f8d37e1e615c9
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-08-20 12:07:40 -0600

    final push

--- blockchain-capstone/src/components/App.js
+++ blockchain-capstone/src/components/App.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react'
-import './App.css'
-import Navbar from './Navbar'
-import Content from './Content'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import Content from "./Content";
+import { connect } from "react-redux";
 import {
   loadWeb3,
   loadAccount,
   loadToken,
-  loadExchange
-} from '../store/interactions'
-import { contractsLoadedSelector } from '../store/selectors'
+  loadExchange,
+} from "../store/interactions";
+import { contractsLoadedSelector } from "../store/selectors";
 
 class App extends Component {
   componentWillMount() {
-    this.loadBlockchainData(this.props.dispatch)
+    this.loadBlockchainData(this.props.dispatch);
   }
 
   async loadBlockchainData(dispatch) {
-    const web3 = await loadWeb3(dispatch)
-    const networkId = await web3.eth.net.getId()
-    await loadAccount(web3, dispatch)
-    const token = await loadToken(web3, networkId, dispatch)
-    if(!token) {
-      window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
-      return
+    const web3 = await loadWeb3(dispatch);
+    const networkId = await web3.eth.net.getId();
+    await loadAccount(web3, dispatch);
+    const token = await loadToken(web3, networkId, dispatch);
+    if (!token) {
+      window.alert(
+        "Token smart contract not detected on the current network. Please select another network with Metamask."
+      );
+      return;
     }
-    const exchange = await loadExchange(web3, networkId, dispatch)
-    if(!exchange) {
-      window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.')
-      return
+    const exchange = await loadExchange(web3, networkId, dispatch);
+    if (!exchange) {
+      window.alert(
+        "Exchange smart contract not detected on the current network. Please select another network with Metamask."
+      );
+      return;
     }
   }
 
@@ -36,7 +40,11 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
+        {this.props.contractsLoaded ? (
+          <Content />
+        ) : (
+          <div className="content"></div>
+        )}
       </div>
     );
   }
@@ -44,8 +52,8 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
-    contractsLoaded: contractsLoadedSelector(state)
-  }
+    contractsLoaded: contractsLoadedSelector(state),
+  };
 }
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
--- blockchain-capstone/truffle-config.js
+++ blockchain-capstone/truffle-config.js
@@ -1,13 +1,29 @@
 require("babel-register");
 require("babel-polyfill");
 require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*", //Match any network id
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          [
+            "5f434f85a4b2df25280073bc5fc171e835176575f9dfbc45826b6b8843a71450",
+            "1543a4250c4c6eb039334f9bf3f66398f12ab5a493afe9d5abf1f4a3696b1031",
+          ], // Array of account private keys
+          "https://rinkeby.infura.io/v3/52474cef7b964b4fbf8e954a5dfa481b" // Url to an Ethereum Node
+        );
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4,
     },
   },
   contracts_directory: "./src/contracts/",
